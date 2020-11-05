c2c8ded23207efe460d408731dadb2e90cd3ffc7
erlingto <erling.tmte@gmail.com>
2020-11-05 12:07:01 +0100

    added image size

--- classifier.py
+++ classifier.py
@@ -420,8 +420,8 @@ def evaluation(Classifier, test_batch_size, prnt):
 def train_classifier(trial):
 
     cfg = {
-        "image_size": (224, 224), (180, 180), (150, 150), (100,100), (300, 300)
-        "learning_rate": trial.suggest_loguniform('lr', 1e-3, 1e-2),  # 0.000134,
+        "image_size":  trial.suggest_categorical('image_size', [(224, 224), (180, 180), (150, 150), (100,100), (300, 300)]), 
+        "learning_rate": trial.suggest_loguniform('lr', 1e-4, 1e-3),  # 0.000134,
         "mini_batch_size": 32,
         "test_batch_size": 100,
         "step_size": 32,
